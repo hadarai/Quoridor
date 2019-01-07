4e4f3d9b613cd4ed6235b438e32ba0e1351e291d
hadarai <309498@uwr.edu.pl>
2019-01-07 17:25:43 +0100

    Initial Commit

---
+++ Quoridor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Quoridor
+//
+//  Created by Maksymilian Czudziak on 07/01/2019.
+//  Copyright © 2019 Maksymilian Czudziak. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
